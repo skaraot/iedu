57405846e5986bdf07c0741c7aab5a4d819cf432
serkan karaot <serkankaraot@hotmail.com>
2015-12-15 10:08:31 +0200

    döngü güncellendi
    For yerine foreach döngüsü geldi

--- hiClone2.php
+++ hiClone2.php
@@ -3,7 +3,10 @@ echo "merhaba git hub";
 echo "<br>";
 echo "deneme";
 $adlar=array('serkan', 'mahmut', 'hamdi', 'taşkın', 'samet');
-for ($i=0;$i<=count($adlar)-1;$i++){
-	echo $adlar[$i];
+foreach ($adlar as $value){
+	echo $value;
 }
+/*for ($i=0;$i<=count($adlar)-1;$i++){
+	echo $adlar[$i];
+}*/
 ?>
