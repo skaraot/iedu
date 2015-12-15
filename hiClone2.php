ef3dffd87825720f84e450b250c41bf53d3677b3
serkan karaot <serkankaraot@hotmail.com>
2015-12-15 11:56:44 +0200

    satır atlama ve toplam sayı belirtildi.

--- hiClone2.php
+++ hiClone2.php
@@ -4,8 +4,9 @@ echo "<br>";
 echo "deneme";
 $adlar=array('serkan', 'mahmut', 'hamdi', 'taşkın', 'samet');
 foreach ($adlar as $value){
-	echo $value;
+	echo $value."<br>";
 }
+echo "<hr>".count($adlar)." adet isim var ";
 /*for ($i=0;$i<=count($adlar)-1;$i++){
 	echo $adlar[$i];
 }*/
